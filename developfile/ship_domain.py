6e5b5285d0722a74dbb1b9aedcf2470904e3156b
yaosailor <yaochenyang@stu.shmtu.edu.cn>
2019-11-08 15:57:15 +0800

    ship domain(Goodwin 1975)

--- developfile/ship_domain.py
+++ developfile/ship_domain.py
@@ -33,9 +33,15 @@ Parameters:
 *    domain_b :  semi-minor axis,             3.5*length
 *    cog:      rotation angle,              cog
 *    
+
 * method 2   Goodwin
-*    ···
-*    
+*    Thita: the relative bearing of other ships 
+*    Sector 1 (starboard sector) 0  <= Thita <= 112.5 
+*    Sector 2 (port sector)      247.5 <= Thita < 360 
+*    Sector 3 (astern sector)    112.5 <= Thita < 247.5
+*    Ref. 
+*    [1] Goodwin, E. (1975). A Statistical Study of Ship Domains. Journal of Navigation, 28(3), 328-344. 
+
 * method 3   wang
 *    Vown: the own ship speed represented in knots.
 *    IQSDk = {(x,y) | fk(x,y;Q) <= 1, Q = {R_fore, R_aft, R_starb, R_port}, k >= 1 }
@@ -82,6 +88,39 @@ def fujidomain(own_ship_xy, length, cog=0):
     domain_path = [own_ship_xy[0] + Ell_rot[0, :], own_ship_xy[1] + Ell_rot[1, :]]
     return domain_path
 
+
+def goodwindomain(own_ship_xy, cog):
+    """
+    Parameters
+    ----------
+    own_ship_xy : (float, float)
+        xy coordinates of ellipse centre.
+    cog : scalar, optional
+        Rotation in degrees anti-clockwise.
+    """
+    thita = np.array([cog, cog + 112.5, cog + 247.5, cog + 360])
+    # number_of_scetor:
+    #   use the number_of_scetor to control the result of sector.
+    #   defalut values is 100, that means every sector have 100 points.
+    #   sector_tem: (x,y), size:(2, number_of_scetor)
+    number_of_scetor = 100
+    radius_domain =[1.852*1000*x for x in [0.85, 0.4, 0.7]]
+    thita_deg = pi * thita / 180
+    sector = np.zeros(shape=(2, 3 * number_of_scetor + 1))
+    # calculate the sectors
+    num_start = 0
+    for jj in range(0, thita_deg.shape[0] - 1):
+        print(num_start)
+        t = np.linspace(thita_deg[jj], thita_deg[jj + 1], number_of_scetor)
+        sector_tem = np.array([radius_domain[jj] * np.sin(t), radius_domain[jj] * np.cos(t)])
+        sector[:, num_start:num_start + number_of_scetor] = sector_tem[:, :]
+        num_start = num_start + number_of_scetor
+    sector[:, 3 * number_of_scetor] = sector[:, 0]
+    sector[0, :] += own_ship_xy[0]
+    sector[1, :] += own_ship_xy[1]
+    ship_sector = sector.copy()
+    return ship_sector
+
 def ship_shap_base(length, width):
     #ship_path = [[0, length/2], [width/2, length/10], [width/2, -length/2],
     #             [-width/2, -length/2], [width/2, -length/10], [0, length/2]]
@@ -105,7 +144,7 @@ if __name__ == '__main__':
     width = 30.0
     ownship_x = 100.0
     ownship_y = 0.5
-    cog = 120
+    cog = 0
     # - process
     ship_path_base = ship_shap_base(length, width)
     ship_path = ship_shape(ship_path_base.T, cog)
@@ -115,16 +154,18 @@ if __name__ == '__main__':
     ship_ver[:, 0] = ship_path[:, 0] + ownship_x
     # calculate the domain
     domain_ship = fujidomain((ownship_x, ownship_y), length, cog)
+    domain_ship_g = goodwindomain((ownship_x, ownship_y), cog)
 
     # ----------------------------#
     #  plot the ship domain
     # ----------------------------#
     path = Path(ship_ver)
-    pathpatch = PathPatch(path, facecolor='None', edgecolor='k')
+    pathpatch = PathPatch(path, facecolor='gray', edgecolor='k')
     #
     fig, ax = plt.subplots()
     ax.add_patch(pathpatch)
     plt.plot(domain_ship[0], domain_ship[1], 'gray' )    # rotated ellipse
+    plt.plot(domain_ship_g[0], domain_ship_g[1], 'blue' )    # rotated ellipse
     plt.grid(color='lightgray', linestyle='--')
     ax.set_aspect(1.0)
     plt.show()
